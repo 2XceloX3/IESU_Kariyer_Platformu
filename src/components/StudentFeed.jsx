c5de8a284f4bed3f576775fab9a3b3d85aa54fdc
IESU Kariyer <kariyer@esenyurt.edu.tr>
2026-07-12 09:50:28 +0300

    Fix Vercel Build: Remove invalid CVBuilder import that crashed the build

--- src/components/StudentFeed.jsx
+++ src/components/StudentFeed.jsx
@@ -16,8 +16,6 @@ import CalendarPlanning from './CalendarPlanning';
 import AICVBuilder from './AICVBuilder';
 import NavIcon from './shared/NavIcon';
 import ClubsDirectory from './ClubsDirectory';
-import CVBuilder from './CVBuilder';
-import StoryViewer from './StoryViewer';
 import CareerShorts from './CareerShorts';
 
 export default function StudentFeed({ setView, setSelectedUserId, notifications = [], setNotifications, posts, setPosts, stories, setStories, surveys, userRole, news, events, students, alumni, companies, currentUser, featuredOpportunities, mentorships, voluntaryInternships, messages, setMessages, applications, setApplications, jobs, academicStaff, announcements, academicRole, groups, setGroups, setSelectedGroupId, featureClubsShowcase, featureClubApplications, clubs, setClubs, clubApplications, setClubApplications }) {
